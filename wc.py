a0b07b22cb31e7d917df9bc58d086678cda82725
Arnold <kamstan6@gmail.com>
2019-10-16 18:07:02 +0100

    handling more input scenarios

--- wc.py
+++ wc.py
@@ -1,15 +1,17 @@
 if __name__ == "__main__":
 	import sys
 	script_name = sys.argv[0]
-	allowed_options = ['-l', '-w', '-c']
+	allowed_options = ['l', 'w', 'c']
+	line_flag = 'l'
+	word_flag = 'w'
+	byte_flag = 'c'
 	results = []
 	total_words = 0
 	total_lines = 0
 	total_bytes = 0
-	one_flag = False
 	no_flag = False
 	multiple_flags = False
-	flag_count = 0
+
 
 	def openfile(file_names, argument):
 		global num_of_lines
@@ -31,28 +33,15 @@ if __name__ == "__main__":
 					total_bytes += f.tell()
 					if len(argument) > 1:
 						global multiple_flags
-						global flag_count
 						multiple_flags = True
-						for i in argument:
-							if i == '-l':
-								flag_count += 1
-								results.extend([num_of_lines])
-							if i == '-w':
-								flag_count += 1
-								results.extend([num_of_words])
-							if i == '-c':
-								flag_count += 1
-								results.extend([f.tell()])
+						if line_flag in argument:
+							results.extend([num_of_lines])
+						if word_flag in argument:
+							results.extend([num_of_words])
+						if byte_flag in argument:
+							results.extend([f.tell()])
 						results.extend([file])
-					elif len(argument) == 1:
-						global one_flag
-						one_flag = True
-						if argument[0] == '-l':
-							results.extend([num_of_lines, file])
-						elif argument[0] == '-w':
-							results.extend([num_of_words, file])
-						elif argument[0] == '-c':
-							results.extend([f.tell(), file])
+
 					else:
 						global no_flag
 						no_flag = True
@@ -62,7 +51,7 @@ if __name__ == "__main__":
 			except OSError:
 				print("miniwc: we don't handle that situation yet!")
 			except UnicodeDecodeError:
-				with open(sys.argv[1], 'rb') as f:
+				with open(file, 'rb') as f:
 					for line in f:
 						num_of_lines+=1
 						line_in_uni = line.decode('latin-1')
@@ -78,26 +67,15 @@ if __name__ == "__main__":
 		if len(file_names) > 1:
 			if no_flag:
 				results.extend([total_lines, total_words, total_bytes, 'total'])
-			elif one_flag:
-				for flag in argument:
-					if flag == '-l':
-						results.extend([total_lines])
-					elif flag == '-c':
-						results.extend([total_bytes])
-					elif flag == '-w':
-						results.extend([total_words])
-					results.extend(['total'])
-			elif multiple_flags:
-				for flag in argument:
-					if flag == '-l':
-						results.extend([total_lines])
-					if flag == '-w':
-						results.extend([total_words])
-					if flag == '-c':
-						results.extend([total_bytes])
+			if multiple_flags:
+				if line_flag in argument:
+					results.extend([total_lines])
+				if word_flag in argument:
+					results.extend([total_words])
+				if byte_flag in argument:
+					results.extend([total_bytes])
 				results.extend(['total'])
 			print_results(results)
-
 	def print_results(results):
 		sorted_integers = sorted([i for i in results if type(i) is int])
 		sorted_file_names = sorted([i for i in results if type(i) is str])
@@ -111,7 +89,8 @@ if __name__ == "__main__":
 	def missing_file_missing():
 		print("Wc: File missing")
 
-	#check for flags in the list
+
+	#check for flags and files in the list
 	if len(sys.argv) == 1:
 		missing_file_missing()
 
@@ -123,16 +102,22 @@ if __name__ == "__main__":
 			flags = [flag for flag in sys.argv if flag[0].lower() == flag_character]
 			last_flag = flags[-1]
 			last_flag_index = sys.argv.index(last_flag)
+			files_in_list = [single_file for single_file in sys.argv[1:] if single_file[0].lower() != flag_character]
 			file_in_argument = sys.argv[last_flag_index+1:]
+			used_flags = []
 			if len(flags) >= 1:
-				if any(i in allowed_options for i in flags):
-					openfile(file_in_argument, flags)
+				for i in flags:
+					for j in i:
+						used_flags.append(j)
+				if any(i in allowed_options for i in used_flags):
+					openfile(file_in_argument, used_flags)
+				else:
+					print("wrong flag used")
 
-			#if sys.argv[1:] in allowed_options:
 			else:
 				print("breaking out")
 				pass
 
 	else:
 		argument = []
-		openfile(sys.argv[1:],argument)
+		openfile(sys.argv[1:], argument)
