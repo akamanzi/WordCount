72b948de8fd3b8d096dda5fcf79e42ef067e7550
Arnold <kamstan6@gmail.com>
2019-11-03 10:15:15 +0000

    1) Added files-from flag 2) Refactored code (Addition of classes and relevant routines)

--- wc.py
+++ wc.py
@@ -17,91 +17,199 @@ total_line_length = 0
 
 no_flag = False
 multiple_flags = False
+is_file_from_flag_present = False
 
-def count_max_line_length(file_name):
-	try:
-		with open(file_name, 'r', encoding='utf-8') as f:
+class Counter:
+
+	def __init__(self, file_content):
+		self.file_content = file_content
+
+	def line_count(self):
+		return len(self.file_content.splitlines())
+
+	def word_count(self):
+		return len(self.file_content.split())
+
+	def byte_count(self):
+		return len(self.file_content)
+
+	def count_max_line_length(self, file_name):
+		try:
+			with open(file_name, 'r', encoding='utf-8') as f:
+				file_content = f.read()
+				line = file_content.split('\n')
+				large_line_len = 0
+				for l in line:
+					if len(l) > large_line_len:
+						large_line_len = len(l)
+		except UnicodeDecodeError:
+			file_handler = FileHandler([], [])
+			large_line_len = file_handler.open_binary_file(file_name)
+		return large_line_len
+
+
+class ArgumentHandler:
+	def handle_arguments(self):
+		# Initialise arguments
+		help_message = """
+		Print newline, word, and byte counts for each FILE, and a total line if
+		more than one FILE is specified.  With no FILE, or when FILE is -,
+		read standard input.  A word is a non-zero-length sequence of characters
+		delimited by white space.\n
+		The options below may be used to select which counts are printed, always in
+		the following order: newline, word, character, byte, maximum line length."""
+
+		parser = argparse.ArgumentParser(description=help_message,
+										 add_help=False)
+		parser.add_argument("-l", help="output number of lines", action='store_true')
+		parser.add_argument("-w", help="output number of words", action='store_true')
+		parser.add_argument("-c", help="output number of bytes", action='store_true')
+		parser.add_argument("--help", help="outputs help message", action='help',
+							default=argparse.SUPPRESS)
+		parser.add_argument("--version", help="outputs version of wc", action='version',
+							version='%(prog)s 1.0')
+		parser.add_argument("-L", help="output length of the longest line", action='store_true')
+		parser.add_argument("-m", help="output number of characters in file", action='store_true')
+		parser.add_argument("--files0-from", action='store_true')
+		parser.add_argument("filename", nargs='+')
+
+		if len(sys.argv) == 1:
+			stdin_content = sys.stdin.read()
+			handle_stdin(stdin_content)
+			sys.exit()
+
+		args = parser.parse_args()
+		flag_list = []
+		list_of_arguments = []
+		if args.l:
+			flag_list.append(line_flag)
+		if args.w:
+			flag_list.append(word_flag)
+		if args.m:
+			flag_list.append(CHAR_FLAG)
+		if args.c:
+			flag_list.append(byte_flag)
+		if args.L:
+			flag_list.append(max_length)
+		if args.files0_from:
+			global is_file_from_flag_present
+			is_file_from_flag_present = True
+
+		list_of_arguments.extend([args.filename, flag_list])
+		return list_of_arguments
+
+
+class FileHandler:
+	def __init__(self, file_names, flags):
+		self.file_names = file_names
+		self.flags = flags
+
+	def openfiles(self):
+		global num_of_lines
+		global num_of_words
+		for file in self.file_names:
+			num_of_lines = 0
+			num_of_words = 0
+			global total_words, total_lines, total_bytes, total_line_length
+
+			try:
+				with open(file, 'rb') as f:
+					file_content = f.read()
+					if is_file_from_flag_present:
+						self.handle_null_terminated_files(file_content, self.flags)
+						break
+					counter = Counter(file_content)
+					num_of_lines = counter.line_count()
+					num_of_words = counter.word_count()
+					byte_count = counter.byte_count()
+					max_line_length = counter.count_max_line_length(file)
+
+					total_words += num_of_words
+					total_lines += num_of_lines
+
+					total_bytes += byte_count
+					if len(self.flags) >= 1:
+						global multiple_flags
+						multiple_flags = True
+						if line_flag in self.flags:
+							results.extend([num_of_lines])
+						if word_flag in self.flags:
+							results.extend([num_of_words])
+						if CHAR_FLAG in self.flags:
+							results.extend([byte_count])
+						if byte_flag in self.flags:
+							results.extend([byte_count])
+						if max_length in self.flags:
+							total_line_length += max_line_length
+							results.extend([max_line_length])
+						results.extend([file])
+
+					else:
+						global no_flag
+						no_flag = True
+						results.extend([num_of_lines, num_of_words, f.tell(), file])
+					print_results(results)
+					results.clear()
+			except OSError:
+				pass
+				#print("we don't handle that situation yet!")
+		if len(self.file_names) > 1:
+			if no_flag:
+				results.extend([total_lines, total_words, total_bytes, 'total'])
+			if multiple_flags:
+				if line_flag in self.flags:
+					results.extend([total_lines])
+				if word_flag in self.flags:
+					results.extend([total_words])
+				if CHAR_FLAG in self.flags:
+					results.extend([total_bytes])
+				if byte_flag in self.flags:
+					results.extend([total_bytes])
+				if max_length in self.flags:
+					results.extend([max_line_length])
+				results.extend(['total'])
+			print_results(results)
+
+	def open_binary_file(self, file_name):
+		with open(file_name, 'rb') as f:
 			file_content = f.read()
-			line = file_content.split('\n')
+			line = file_content.splitlines()
 			large_line_len = 0
 			for l in line:
 				if len(l) > large_line_len:
 					large_line_len = len(l)
-	except UnicodeDecodeError:
-		large_line_len = open_binary_file(file_name)
-	return large_line_len
+		return large_line_len
 
-def open_binary_file(file_name):
-	with open(file_name, 'rb') as f:
-		file_content = f.read()
-		line = file_content.splitlines()
-		large_line_len = 0
-		for l in line:
-			if len(l) > large_line_len:
-				large_line_len = len(l)
-	return large_line_len
+	def handle_null_terminated_files(self, file_content, flags):
+		global is_file_from_flag_present
+		if self.is_line_null_terminated(file_content):
+			# check if the file has only 1 word
+			if len(file_content.split(b'\n')) == 1 and len(file_content.split()) == 1 and len(file_content) > 1:
+				file_path = []
+				for line in file_content.split(b'\x00'):
+					file_path.append(line.decode('utf-8'))
+				is_file_from_flag_present = False
+				self.file_names = file_path
+				self.flags = flags
+				self.openfiles()
+				#openfile(file_path, flags)
+			else:
+				for line in file_content.split(b'\n'):
+					print(line.decode('utf-8'))
 
-def openfile(file_names, argument):
-	global num_of_lines
-	global num_of_words
+		else:
+			for line in file_content.split(b'\n'):
+				print(line.decode('utf-8'))
 
-	for file in file_names:
-		num_of_lines = 0
-		num_of_words = 0
-		global total_words, total_lines, total_bytes, total_line_length
+	def is_line_null_terminated(self, file):
+		if b'\x00' in file:
+			return True
+		else:
+			return False
+	# for single_line in file_content.split(b'\x00'):
+	# 	print(single_line)
+	# 	print(len(single_line.split()))
 
-		try:
-			with open(file, 'rb') as f:
-				file_content = f.read()
-				# TODO: isolate counting
-				num_of_lines = len(file_content.splitlines())
-				num_of_words = len(file_content.split())
-				total_words += num_of_words
-				total_lines += num_of_lines
-
-				total_bytes += f.tell()
-				if len(argument) >= 1:
-					global multiple_flags
-					multiple_flags = True
-					if line_flag in argument:
-						results.extend([num_of_lines])
-					if word_flag in argument:
-						results.extend([num_of_words])
-					if CHAR_FLAG in argument:
-						results.extend([f.tell()])
-					if byte_flag in argument:
-						results.extend([f.tell()])
-					if max_length in argument:
-						max_line_length = count_max_line_length(file)
-						total_line_length += max_line_length
-						results.extend([max_line_length])
-					results.extend([file])
-
-				else:
-					global no_flag
-					no_flag = True
-					results.extend([num_of_lines, num_of_words, f.tell(), file])
-				print_results(results)
-				results.clear()
-		except OSError:
-			print("we don't handle that situation yet!")
-	if len(file_names) > 1:
-		if no_flag:
-			results.extend([total_lines, total_words, total_bytes, 'total'])
-		if multiple_flags:
-			if line_flag in argument:
-				results.extend([total_lines])
-			if word_flag in argument:
-				results.extend([total_words])
-			if CHAR_FLAG in argument:
-				results.extend([total_bytes])
-			if byte_flag in argument:
-				results.extend([total_bytes])
-			if max_length in argument:
-				results.extend([max_line_length])
-			results.extend(['total'])
-		print_results(results)
-	#return results
 
 def print_results(results):
 	for result in results:
@@ -121,56 +229,12 @@ def handle_stdin(input):
 	print('\t', line_count, '\t', word_count, '\t', byte_count)
 
 # TODO: pass arguments and return list of results
-def handle_arguments():
-	# Initialise arguments
-	help_message = """
-	Print newline, word, and byte counts for each FILE, and a total line if
-	more than one FILE is specified.  With no FILE, or when FILE is -,
-	read standard input.  A word is a non-zero-length sequence of characters
-	delimited by white space.\n
-	The options below may be used to select which counts are printed, always in
-	the following order: newline, word, character, byte, maximum line length."""
-
-	parser = argparse.ArgumentParser(description=help_message,
-									 add_help=False)
-	parser.add_argument("-l", help="output number of lines", action='store_true')
-	parser.add_argument("-w", help="output number of words", action='store_true')
-	parser.add_argument("-c", help="output number of bytes", action='store_true')
-	parser.add_argument("--help", help="outputs help message", action='help',
-						default=argparse.SUPPRESS)
-	parser.add_argument("--version",  help="outputs version of wc", action='version',
-						version='%(prog)s 1.0')
-	parser.add_argument("-L", help="output length of the longest line", action='store_true')
-	parser.add_argument("-m", help="output number of characters in file", action='store_true')
-	parser.add_argument("filename", nargs='+')
-
-	if len(sys.argv) == 1:
-		stdin_content = sys.stdin.read()
-		handle_stdin(stdin_content)
-		sys.exit()
-
-	args = parser.parse_args()
-	flag_list = []
-	list_of_arguments = []
-	if args.l:
-		flag_list.append(line_flag)
-	if args.w:
-		flag_list.append(word_flag)
-	if args.m:
-		flag_list.append(CHAR_FLAG)
-	if args.c:
-		flag_list.append(byte_flag)
-	if args.L:
-		flag_list.append(max_length)
-
-	# print(args.filename, flag_list)
-	#openfile(args.filename, flag_list)
-	list_of_arguments.extend([args.filename, flag_list])
-	return list_of_arguments
 
 if __name__ == "__main__":
-	parsed_arguments_list = handle_arguments()
+	list_of_arguments = ArgumentHandler()
+	parsed_arguments_list = list_of_arguments.handle_arguments()
+	file_handler = FileHandler(parsed_arguments_list[0], parsed_arguments_list[1])
+	file_handler.openfiles()
 	#print(parsed_arguments_list[0], parsed_arguments_list[1])
-	openfile(parsed_arguments_list[0], parsed_arguments_list[1])
-	#print(output)
-	#print_results(output)
+	#openfile(parsed_arguments_list[0], parsed_arguments_list[1])
+
